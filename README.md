4e6bbe397fd878a0b96a22383645ef9f1343bf10
Jérémie Zarca <jeremie.zarca@gmail.com>
2018-12-27 01:05:28 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,15 @@
+# InstaCLI
+
+A Command-Line Interface for Instagram Upload
+
+## Installation
+
+```shell
+npm install --global instacli
+```
+
+## Usage
+
+```shell
+insta
+```
---
+++ index.js
@@ -0,0 +1,92 @@
+#!/usr/bin/env node
+
+const chalk = require('chalk');
+const clear = require('clear');
+const figlet = require('figlet');
+const fs = require('fs');
+const CLI = require('clui');
+const Spinner = CLI.Spinner;
+const args = require('minimist')(process.argv.slice(2));
+
+const log = console.log;
+
+const Instagram = require('instagram-web-api');
+const FileCookieStore = require('tough-cookie-filestore2');
+const COOKIE_PATH = './cookies.json';
+
+const Hashtagify = require('node-hashtagify');
+const hash = new Hashtagify();
+
+const { askInstaCredentials, askUploadDetails } = require('./lib/inquirer');
+const {
+  isCredentialsSet,
+  getCredentials,
+  resetCredentials,
+  setCredentials
+} = require('./lib/credentials');
+
+clear();
+console.log(
+  chalk.yellow(figlet.textSync('#InstaCLI', { horizontalLayout: 'full' }))
+);
+
+const run = async () => {
+  if (!isCredentialsSet()) {
+    const credentials = await askInstaCredentials();
+    setCredentials(credentials);
+  }
+  const details = await askUploadDetails();
+  uploadToInstagram(details);
+};
+
+const uploadToInstagram = async details => {
+  let spinner = new Spinner('Loading...');
+  try {
+    if (fs.existsSync(COOKIE_PATH)) {
+      fs.unlink(COOKIE_PATH);
+    }
+    const cookieStore = new FileCookieStore(COOKIE_PATH);
+    const client = new Instagram(getCredentials(), cookieStore);
+
+    let caption = details.caption;
+    const hashtags = details.hashtags.replace(/\s/g, '').split(',');
+
+    if (hashtags.length) {
+      spinner = new Spinner('Generating hashtags...');
+      spinner.start();
+      const related = await hash.getRelated(hashtags);
+      related.map(tag => {
+        return tag.related.map(
+          hashtag => (caption = caption + ` #${hashtag.name}`)
+        );
+      });
+      spinner.stop();
+    }
+
+    spinner = new Spinner('Logging into Instagram account...');
+    spinner.start();
+    await client.login();
+    spinner.stop();
+
+    spinner = new Spinner('Uploading photo...');
+    spinner.start();
+    const { media } = await client.uploadPhoto({
+      photo: details.filename,
+      caption
+    });
+    spinner.stop();
+
+    log(
+      chalk.green(`Photo uploaded : https://www.instagram.com/p/${media.code}/`)
+    );
+  } catch (err) {
+    spinner.stop();
+    log(chalk.red(err));
+  }
+};
+
+if (args.reset) {
+  log(chalk.green(`Credentials reset`));
+  return resetCredentials();
+}
+return run();
